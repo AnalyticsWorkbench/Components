1a4246e152babdd9bd58bdc6e2553379ecec4983
Freddy29 <aprin.farbod@gmail.com>
2018-10-25 16:29:03 +0200

    Handle null pointer in CentrallityAgent for the checkboxes - over check the JSON fields - Clean

--- components-centrality/src/main/java/eu/sisob/components/centrality/CentralityAgent.java
+++ components-centrality/src/main/java/eu/sisob/components/centrality/CentralityAgent.java
@@ -10,68 +10,76 @@ import java.util.logging.Level;
 
 public class CentralityAgent extends RWrapperAgent {
 
-	String scriptPrefix = "eu/sisob/components/centrality/";
-	private boolean keepIds;
-	private boolean extendedMetadata;
+    String scriptPrefix = "eu/sisob/components/centrality/";
+    private boolean keepIds;
+    private boolean extendedMetadata;
 
-	public CentralityAgent(JsonObject commandMsg) {
-		super(commandMsg);
 
-		String scriptSelectionObject = commandMsg.get("parameters").getAsString();
-		JsonObject jsonObject = new Gson().fromJson(scriptSelectionObject, JsonObject.class);
-		String scriptSelection = jsonObject.get("value1").getAsString();
+    public CentralityAgent(JsonObject commandMsg) {
+        super(commandMsg);
 
-		String scriptName = "undirectedgraph_degree_centrality.R";
-		System.out.printf(scriptName);
-		System.out.printf("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-		if (scriptSelection.equals("Degree")) {
-			scriptName = "undirectedgraph_degree_centrality.R";
-		} else if (scriptSelection.equals("Betweenness")) {
-			scriptName = "undirectedgraph_betweenness_centrality.R";
-		} else if (scriptSelection.equals("Closeness")) {
-			scriptName = "undirectedgraph_closeness_centrality.R";
-		} else if (scriptSelection.equals("Indegree")) {
-			scriptName = "in_degree_centrality.R";
-		} else if (scriptSelection.equals("Outdegree")) {
-			scriptName = "out_degree_centrality.R";
-		} else if (scriptSelection.equals("Directed Betweenness")) {
-			scriptName = "directedgraph_betweenness_centrality.R";
-		} else if (scriptSelection.equals("Directed Closeness")) {
-			scriptName = "directedgraph_closeness_centrality.R";
-		} else if (scriptSelection.equals("Eigenvector")) {
-			scriptName = "eigenvector_centrality.R";
-		} else if (scriptSelection.equals("Strength (weighted Degree)")) {
-			scriptName = "undirectedgraph_degree_strength.R";
-		} else if (scriptSelection.equals("Incoming Strength (weighted Indegree)")) {
-			scriptName = "in_degree_strength.R";
-		} else if (scriptSelection.equals("Outgoing Strength (weighted Outdegree)")) {
-			scriptName = "out_degree_strength.R";
-		} else if (scriptSelection.equals("0")){
-			scriptSelection = "Degree";
-			scriptName = "undirectedgraph_degree_centrality.R";
-			logger.log(Level.WARNING, "could not find the selected script ++++++ the user choiced no centrality filetr! +++ Default is Degree-centrality");
-		}
+        String scriptSelectionObject = commandMsg.get("parameters").getAsString();
+        JsonObject jsonObject = new Gson().fromJson(scriptSelectionObject, JsonObject.class);
+        String scriptSelection = jsonObject.get("value1").getAsString();
+        if (jsonObject.has("value2")) {
+            keepIds = jsonObject.get("value2").getAsBoolean();
+        } else {
+            keepIds = true;
+            logger.log(Level.WARNING, "could not find the selected KeepID ++++++ the user deosn't check for checkbox");
+        }
+        if (jsonObject.has("value3")) {
+            extendedMetadata = jsonObject.get("value3").getAsBoolean();
+            logger.log(Level.WARNING, "could not find the selected extendedMetadata ++++++ the user deosn't check for checkbox");
+        } else {
+            extendedMetadata = true;
+        }
+        String scriptName = "undirectedgraph_degree_centrality.R";
+        System.out.printf(scriptName);
+        if (scriptSelection.equals("Degree")) {
+            scriptName = "undirectedgraph_degree_centrality.R";
+        } else if (scriptSelection.equals("Betweenness")) {
+            scriptName = "undirectedgraph_betweenness_centrality.R";
+        } else if (scriptSelection.equals("Closeness")) {
+            scriptName = "undirectedgraph_closeness_centrality.R";
+        } else if (scriptSelection.equals("Indegree")) {
+            scriptName = "in_degree_centrality.R";
+        } else if (scriptSelection.equals("Outdegree")) {
+            scriptName = "out_degree_centrality.R";
+        } else if (scriptSelection.equals("Directed Betweenness")) {
+            scriptName = "directedgraph_betweenness_centrality.R";
+        } else if (scriptSelection.equals("Directed Closeness")) {
+            scriptName = "directedgraph_closeness_centrality.R";
+        } else if (scriptSelection.equals("Eigenvector")) {
+            scriptName = "eigenvector_centrality.R";
+        } else if (scriptSelection.equals("Strength (weighted Degree)")) {
+            scriptName = "undirectedgraph_degree_strength.R";
+        } else if (scriptSelection.equals("Incoming Strength (weighted Indegree)")) {
+            scriptName = "in_degree_strength.R";
+        } else if (scriptSelection.equals("Outgoing Strength (weighted Outdegree)")) {
+            scriptName = "out_degree_strength.R";
+        } else if (scriptSelection.equals("0")) {
+            scriptSelection = "Degree";
+            scriptName = "undirectedgraph_degree_centrality.R";
+            logger.log(Level.WARNING, "could not find the selected script ++++++ the user choiced no centrality filetr! +++ Default is Degree-centrality");
+        }
 
-		if (scriptName != null) {
-			this.scriptStream = ClassLoaderUtility.getClassLoader().getResourceAsStream(scriptPrefix + scriptName);
-		} else {
-			indicateError("could not find the selected script");
-		}
+        if (scriptName != null) { // FBA never happen always true / we can handle null in front too ReDux
+            this.scriptStream = ClassLoaderUtility.getClassLoader().getResourceAsStream(scriptPrefix + scriptName);
+        } else {
+            indicateError("could not find the selected script");
+        }
+    }
 
-		keepIds = jsonObject.get("value2").getAsBoolean();
-		extendedMetadata = jsonObject.get("value3").getAsBoolean();
-	}
+    @Override
+    public void executeAgent(JsonObject dataMessage) {
+        logger.fine(agentName + ": agent");
 
-	@Override
-	public void executeAgent(JsonObject dataMessage) {
-		logger.fine(agentName + ": agent");
+        this.input = new Gson().toJson(dataMessage.get("payload"));
+        if (this.prepareAgentWorkspace(this.input, keepIds, extendedMetadata)) {
 
-		this.input = new Gson().toJson(dataMessage.get("payload"));
-		if (this.prepareAgentWorkspace(this.input, keepIds, extendedMetadata)) {
-
-			if (this.executeRScript(null, keepIds)) {
-				this.uploadResults();
-			}
-		}
-	}
+            if (this.executeRScript(null, keepIds)) {
+                this.uploadResults();
+            }
+        }
+    }
 }
